aa7e5157c19d22ea7f2693f24f2ccf64ffd9ada6
X1AOYu233 <1966230402@qq.com>
2018-11-25 00:24:59 +0800

    增加Python的源码

---
+++ Python/main.py
@@ -0,0 +1,76 @@
+import time
+import requests as req
+
+
+def add_common_info(info):
+    now_time = time.strftime('%H:%M:%S /', time.localtime(time.time()))
+    print('[' + now_time + 'INFO] ' + info)
+
+
+# 获得HTML
+def get_bilibili_html(av_url):
+    # 设置头文件，应对反爬措施
+    headers = {
+		'Cookie': 'buvid3=00000000-0000-0000-0000-00000000000000000000000;2; '
+                  ' im_notify_type_1=1',
+    }
+
+    # 直接返回HTML代码
+    return req.get(av_url, headers=headers).text
+
+
+# 通过find方法找到视频下载链接
+def get_download_url(video_url_html):
+    # find这个URL开头
+    video_url_html = video_url_html[video_url_html.find('http://upos-hz-mirrorcos.acgvideo.com/'):]
+    # find结尾
+    return video_url_html[:video_url_html.find('"]')]
+
+
+# 下载器，单线程
+def download_bilibili_video(url, video_url):
+    # 同样要头文件
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
+        'Referer': video_url,   # 需要视频链接
+        'Origin': 'https://www.bilibili.com'
+    }
+    return req.get(url, headers=header).content     # 返回数据流
+
+
+def get_video_name(html):
+    start = html.find('<title data-vue-meta="true">') + 28
+    html = html[start:]
+    return html[:html.find('_哔哩哔哩 ')]
+
+
+def main():
+    print('\t欢迎使用：当前程序处于测试阶段\n\t版本号[alpha 1.0.0]')
+    print('############################################################')
+    add_common_info('程序开始运行')
+    url = 'https://www.bilibili.com/video/' + input('请输入AV号')
+    file_name = input('请输入想要命名的文件名')
+    add_common_info('正在获取视频')
+    file = download_bilibili_video(get_download_url(get_bilibili_html(url)), url)
+    add_common_info('视频获取成功')
+    with open(file_name, 'wb') as f:
+        add_common_info('开始写入文件')
+        f.write(file)
+    add_common_info('写入成功')
+    add_common_info('程序成功执行')
+
+
+if __name__ == '__main__':
+    # main()
+    headers = {
+		'Cookie': 'buvid3=00000000-0000-0000-0000-00000000000000000000000;2; '
+                  ' im_notify_type_1=1',
+    }
+    url = 'https://www.bilibili.com/video/' + input('请输入AV号')
+    print('一')
+    html = req.get(url, headers=headers).text
+    print(html)
+    print('######################################################################')
+    print(get_video_name(html))
+    print('end')
+    time.sleep(1000)
